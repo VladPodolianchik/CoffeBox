c325f2516d1b3809cdcf8c57fc0bcf1cc6ad6bfa
VladPodolianchik <vlad.podolianchik@gmail.com>
2021-03-22 00:20:36 +0300

    Just for prototype

---
+++ PinAndService.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#define Pin 1234
+
+using namespace std;
+
+void enterServicePin(int enteredPin);
+void showServiceBalance();
+void showCupsBalance();
+int cupsReplenishment();	
+int proceedsDispose();		
+
+int main()
+{
+	int temp1 = 0;
+	
+	enterServicePin(temp1); 
+	
+	// in main will be describe output service menu options and relevant function call
+	
+	return 0;
+}
+
+void enterServicePin(int enteredPin)  // 3 input attempts otherwise lock
+
+void showServiceBalance()			
+
+void showCupsBalance()				
+
+int cupsReplenishment()				
+
+int proceedsDispose()				
